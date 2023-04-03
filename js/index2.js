41f6fe088165461ab0074ff14b965aa6359bbf05
Alexssandro <22282169@CFP127ALUNO.LOCAL>
2023-04-03 16:56:45 -0300

    status funcionando

---
+++ js/index2.js
@@ -0,0 +1,70 @@
+'use strict'
+
+import { getAlunosCurso } from './apicursos.js'
+const jsonAlunosCurso = await getAlunosCurso()
+const nomeCurso = document.getElementById('curso_name')
+const status = document.getElementById('status')
+const finalizado = document.getElementById('finalizado')
+const cursando = document.getElementById('cursando')
+
+const criaCardAlunos = (aluno) => {
+
+    nomeCurso.innerHTML = aluno.curso.slice(6)
+
+    const container = document.createElement('div')
+    container.classList.add('container')
+
+    const cardAlunos = document.createElement('div')
+    cardAlunos.classList.add('card_alunos')
+
+    if (aluno.status == 'Cursando') {
+        cardAlunos.classList.add('cursando')
+    } else {
+        cardAlunos.classList.add('finalizado')
+    }
+
+    finalizado.addEventListener('click', () => {
+        if (cardAlunos.classList.value == 'card_alunos cursando') {
+            cardAlunos.style.display = 'none'
+        } else {
+            cardAlunos.style.display = 'flex'
+        }
+    })
+
+    cursando.addEventListener('click', () => {
+        if (cardAlunos.classList.value == 'card_alunos finalizado') {
+            cardAlunos.style.display = 'none'
+        } else {
+            cardAlunos.style.display = 'flex'
+        }
+    })
+    
+
+    const containerAlunos = document.createElement('div')
+    containerAlunos.classList.add('container_card_alunos')
+
+    const image = document.createElement('img')
+    image.src = `${aluno.foto}`
+    image.alt = 'foto usuário'
+
+    const nome = document.createElement('span')
+    nome.textContent = aluno.nome
+
+    container.append(cardAlunos)
+    cardAlunos.append(containerAlunos)
+    containerAlunos.append(image, nome)
+
+
+    return cardAlunos
+
+}
+
+const carregarAlunos = () => {
+    const container = document.getElementById('container')
+    const alunos = jsonAlunosCurso.aluno.map(criaCardAlunos)
+
+    container.replaceChildren(...alunos)
+}
+
+carregarAlunos()
+
